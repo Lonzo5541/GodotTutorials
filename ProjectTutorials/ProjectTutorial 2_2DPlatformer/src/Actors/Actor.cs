607fabe10bbad2687cccbb914fdc70dfa535f381
Lonzo5541 <98492095+Lonzo5541@users.noreply.github.com>
2022-07-31 17:05:38 -0400

    switch to vs code

--- ProjectTutorials/ProjectTutorial 2_2DPlatformer/src/Actors/Actor.cs
+++ ProjectTutorials/ProjectTutorial 2_2DPlatformer/src/Actors/Actor.cs
@@ -1,15 +1,24 @@
 using Godot;
 using System;
 
-public class Player : Actor
+public class Actor : KinematicBody2D
 {
-	
-	
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		
+	}
+	
+	public void _physics_process(float delta){
+		
+		Vector2 vec = new Vector2(300, 0);
+		
+		MoveAndSlide(vec);
+	
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
