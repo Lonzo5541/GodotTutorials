afe137c4d691534bfeb9f81fe7cbbb0d1c9b06fa
Lonzo5541 <98492095+Lonzo5541@users.noreply.github.com>
2022-07-31 21:45:27 -0400

    added player movement

--- ProjectTutorials/ProjectTutorial 2_2DPlatformer/src/Actors/Actor.cs
+++ ProjectTutorials/ProjectTutorial 2_2DPlatformer/src/Actors/Actor.cs
@@ -3,19 +3,19 @@ using System;
 
 public class Actor : KinematicBody2D
 {
-	decimal gravity = 3000.0;
+	Vector2 velocity = Vector2.Zero; //actor movement vector
+	float gravity = 500; //gravity strength
+	Vector2 speed = new Vector2(300, 1000); //actor movement speed
 
 	public override void _Ready(){
 		
 	}
 	
-	public void _PhysicsProcess(float delta){
-		
-		Vector2 velocity = new Vector2.Zero;
+	public void _physics_process(float delta){
 
-		velocity.y += gravity * delta;
+		velocity.y += gravity * delta; //applied gravity on actor
 
-		MoveAndSlide(velocity);
+		velocity = MoveAndSlide(velocity); //actor moves and slides in a given direction
 	}
 
 }
