71dee35da7e2cf5d15bc78b558d5bc5fecfd1ac4
Lonzo5541 <98492095+Lonzo5541@users.noreply.github.com>
2022-08-03 15:24:52 -0400

    added level and player movement

--- ProjectTutorials/ProjectTutorial 2_2DPlatformer/src/Actors/Actor.cs
+++ ProjectTutorials/ProjectTutorial 2_2DPlatformer/src/Actors/Actor.cs
@@ -3,17 +3,26 @@ using System;
 
 public class Actor : KinematicBody2D
 {
-	Vector2 velocity = Vector2.Zero; //actor movement vector
-	float gravity = 500; //gravity strength
-	Vector2 speed = new Vector2(300, 1000); //actor movement speed
+
+	//actor movement vector
+	public Vector2 velocity = Vector2.Zero; 
+
+	//gravity strength
+	public float gravity = 500; 
+
+	//actor movement speed
+	public Vector2 speed = new Vector2(300, 1000); 
 
 	public override void _Ready(){
 		
 	}
-	
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	//Physics process @param float delta
 	public void _physics_process(float delta){
 
-		velocity.y += gravity * delta; //applied gravity on actor
+		//applied gravity on actor
+		velocity.y += gravity * delta; 
 
 		velocity = MoveAndSlide(velocity); //actor moves and slides in a given direction
 	}
--- ProjectTutorials/ProjectTutorial 2_2DPlatformer/src/Actors/Player.cs
+++ ProjectTutorials/ProjectTutorial 2_2DPlatformer/src/Actors/Player.cs
@@ -4,17 +4,38 @@ using System;
 public class Player : Actor
 {
 	
-	
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	//Physics @param float delta
+	public void _process(float delta){
+		
+		//var for jump input
+		int jump;
+
+		//speed of the player
+		speed = new Vector2(800, 1000);
+
+		//checks to
+		if (Input.IsActionPressed("jump")) {
+			
+			jump = -1;
+
+		} else {
+
+			jump = 1;
+		}
+		
+		// The player's movement vector
+		// returns -1 if moving left, 1 if moving right, 0 if not moving or both keys are pressed
+		Vector2 direction = new Vector2(Input.GetActionStrength("move_right") - Input.GetActionRawStrength("move_left"), jump);
+
+		velocity = speed * direction;
+
+	}
+
 }
